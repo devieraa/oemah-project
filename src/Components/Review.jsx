ddb03a8a75e4830d2dce0af8c38ea8ffce518c10
devieraa <devi.rahmaniaputri@gmail.com>
2024-11-24 19:50:49 +0700

    add review 2

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -203,7 +203,7 @@ function Review() {
                                     <h4 className="font-semibold ml-2">Lorem ipsum dolor sit amet</h4>
                                 </div>
                                 <p className="text-gray-700 text-sm">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Halo...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                                 </p>
                             </div>
                         </div>
